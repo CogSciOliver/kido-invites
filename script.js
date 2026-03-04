532ded544bb04ccd7922b77ba7fdccd39381c3ad
chiva-emdr <danii@chivaemdr.com>
2026-03-04 11:57:17 -0600

    updated refresh during pre-event to 50 minutes and attempted font updates

--- script.js
+++ script.js
@@ -204,4 +204,7 @@ END:VCALENDAR
 
 setInterval(() => {
   location.reload();
-}, 60000);
+}, 3000000);
+
+// 3000000 ms = 5 minutes refresh for pre-event updates
+// 60000 ms = 1 minute refresh during live event
